5263ed04ac8f49260496ddd193f62045675772a7
Phandal <bfeld.111@gmail.com>
2024-03-12 16:40:51 -0500

    worked on the arg parser

--- arg/parser.go
+++ arg/parser.go
@@ -1,18 +1,30 @@
 package arg
 
-import "fmt"
+import (
+	"errors"
+)
 
 type Flag struct {
-	name        string
-	value       string
-	description string
-}
-
-type SubCommand struct {
 	name  string
-	flags []Flag
+	value string
 }
 
-func HelloName(name string) string {
-	return fmt.Sprintf("Hello %v!", name)
+type Command struct {
+	Resource string
+	Action   string
+	Options  []Flag
+}
+
+func Parse(args []string) (Command, error) {
+	if len(args) < 3 {
+		return Command{}, errors.New("Invalid Number of Arguements")
+	}
+
+	cmd := Command{
+		Resource: args[1],
+		Action:   args[2],
+		Options:  []Flag{},
+	}
+
+	return cmd, nil
 }
--- celigo.go
+++ celigo.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/Phandal/celigo-cli/arg"
+	"os"
 )
 
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println("\tceligo <resource> <action> [options]")
+}
+
 func main() {
-	fmt.Println(arg.HelloName("Bailey"))
+	cmd, err := arg.Parse(os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		usage()
+		os.Exit(1)
+	}
+
+	fmt.Printf("Resource: %v\n", cmd.Resource)
+	fmt.Printf("Action: %v\n", cmd.Action)
 }
